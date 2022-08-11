7b168a74a7788c0b92456421b4b641dbc52627e9
Raheel Junaid <60077374+raheelrjunaid@users.noreply.github.com>
2022-08-10 18:20:05 -0600

    Make footer dark mode compatible

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 export const Footer = () => (
-  <div className="bg-black/10 w-full">
+  <div className="bg-black/10 w-full dark:bg-white/10 dark:text-white">
     <div className="container text-sm text-center py-1">
       <p>Copywrite {new Date().getFullYear()} &#169; Dua Junaid</p>
     </div>
